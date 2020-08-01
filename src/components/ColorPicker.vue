1b6fc55ecb629263b0100339c2d399852d57bb87
Sergey Adrejew <sergeyandrieiev@gmail.com>
2020-08-01 21:15:32 +0200

    Implement ColorPicker and Modal components

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="color-picker-items-container">
+        <div v-for="color in availableColors" class="color-picker-item" v-bind:style="{backgroundColor: color}" v-bind:key="color" v-on:clicl="onColorSelected(color)">
+            <span className="color-picker-ok" v-bind:style="{ display: selectedColor === color ? 'inline' : 'none'}">&#10004;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ColorPicker',
+    props: {
+        selectedColor: String
+    },
+    data() {
+        return {
+            availableColors: ['#8ED1FC', '#0693E3', '#ABB8C3', '#00D084', '#7BDCB5', '#FCB900', '#FF6900']
+        }
+    },
+    methods: {
+        onColorSelected(value) {
+            this.$emit('color-selected', value);
+        }
+    }
+}
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="modal">
+        <div class="modal-content">
+            <span class="close-btn" v-on:click="onCloseRequest">&times;</span>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    methods: {
+        onCloseRequest() {
+            this.$emit('close-request');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.modal {
+    display: block;
+    position: fixed;
+    z-index: 100;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;    
+}
+
+.modal-content {
+    margin: auto;
+    padding: 30px 2px 8px;
+    width: 30%;
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.2);
+    position: relative;
+}
+
+/* The Close Button */
+.close-btn {
+    color: #aaa;
+    font-size: 28px;
+    display: inline-block;
+    position: absolute;
+    top: -2px;
+    right: 2px;
+}
+  
+.close-btn:hover,
+.close-btn:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+</style>
